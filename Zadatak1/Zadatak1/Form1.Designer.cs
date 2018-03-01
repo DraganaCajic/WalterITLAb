5b8ff5a9018fe2b7aba688b4aa81cba48e14aa32
DraganaCajic <draganacajic@yahoo.com>
2018-03-01 16:08:19 +0100

    Prva verzija!

---
+++ Zadatak1/Zadatak1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Zadatak1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.gbDetails = new System.Windows.Forms.GroupBox();
+            this.rtbDetails = new System.Windows.Forms.RichTextBox();
+            this.gbList = new System.Windows.Forms.GroupBox();
+            this.lbMovie = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pb = new System.Windows.Forms.PictureBox();
+            this.rtbPlot = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.gbDetails.SuspendLayout();
+            this.gbList.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.Controls.Add(this.gbDetails, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.gbList, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 51);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(623, 273);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // gbDetails
+            // 
+            this.gbDetails.Controls.Add(this.rtbPlot);
+            this.gbDetails.Controls.Add(this.pb);
+            this.gbDetails.Controls.Add(this.rtbDetails);
+            this.gbDetails.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbDetails.Location = new System.Drawing.Point(189, 3);
+            this.gbDetails.Name = "gbDetails";
+            this.gbDetails.Size = new System.Drawing.Size(431, 267);
+            this.gbDetails.TabIndex = 2;
+            this.gbDetails.TabStop = false;
+            this.gbDetails.Text = "Details";
+            // 
+            // rtbDetails
+            // 
+            this.rtbDetails.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbDetails.Location = new System.Drawing.Point(163, 16);
+            this.rtbDetails.Name = "rtbDetails";
+            this.rtbDetails.Size = new System.Drawing.Size(262, 127);
+            this.rtbDetails.TabIndex = 0;
+            this.rtbDetails.Text = "";
+            // 
+            // gbList
+            // 
+            this.gbList.Controls.Add(this.lbMovie);
+            this.gbList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbList.Location = new System.Drawing.Point(3, 3);
+            this.gbList.Name = "gbList";
+            this.gbList.Size = new System.Drawing.Size(180, 267);
+            this.gbList.TabIndex = 0;
+            this.gbList.TabStop = false;
+            this.gbList.Text = "List";
+            // 
+            // lbMovie
+            // 
+            this.lbMovie.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbMovie.FormattingEnabled = true;
+            this.lbMovie.Location = new System.Drawing.Point(3, 16);
+            this.lbMovie.Name = "lbMovie";
+            this.lbMovie.Size = new System.Drawing.Size(174, 248);
+            this.lbMovie.TabIndex = 2;
+            this.lbMovie.SelectedValueChanged += new System.EventHandler(this.lbMovie_SelectedValueChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.tbName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(623, 44);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.Location = new System.Drawing.Point(525, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(84, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // tbName
+            // 
+            this.tbName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbName.Location = new System.Drawing.Point(51, 15);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(468, 20);
+            this.tbName.TabIndex = 1;
+            this.tbName.TextChanged += new System.EventHandler(this.tbName_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // pb
+            // 
+            this.pb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pb.Location = new System.Drawing.Point(6, 16);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(151, 127);
+            this.pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb.TabIndex = 1;
+            this.pb.TabStop = false;
+            // 
+            // rtbPlot
+            // 
+            this.rtbPlot.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbPlot.Location = new System.Drawing.Point(6, 149);
+            this.rtbPlot.Name = "rtbPlot";
+            this.rtbPlot.Size = new System.Drawing.Size(419, 115);
+            this.rtbPlot.TabIndex = 2;
+            this.rtbPlot.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 325);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.gbDetails.ResumeLayout(false);
+            this.gbList.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox gbList;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox gbDetails;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbDetails;
+        private System.Windows.Forms.ListBox lbMovie;
+        private System.Windows.Forms.PictureBox pb;
+        private System.Windows.Forms.RichTextBox rtbPlot;
+    }
+}
+
---
+++ Zadatak1/Zadatak1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using Newtonsoft.Json;
+using System.IO;
+using SimpleJson;
+
+namespace Zadatak1
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            btnSearch.Enabled = false;
+            tbName.Focus();
+
+        }
+
+       
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            lbMovie.Items.Clear();
+            pb.ImageLocation = "";
+            rtbDetails.Text = "";
+            rtbPlot.Text = "";
+            RestClient restClient = new RestClient($"http://www.omdbapi.com/?apikey=98f50a84&");
+            RestRequest restRequest = new RestRequest(Method.GET);
+            restRequest.AddParameter("t", tbName.Text.ToString());
+            //RestResponse<Person> response2 = client.Execute<Person>(request);
+            List<Movie> movie;
+            RestSharp.Deserializers.JsonDeserializer deserial = new RestSharp.Deserializers.JsonDeserializer();
+            var response = restClient.Execute(restRequest);
+            movie = deserial.Deserialize<List<Movie>>(response);
+            string data = response.Content;
+            File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\tmpFile.txt", data);
+
+
+
+            foreach (var item in movie)
+            {
+                
+                if (item.Response == "True")
+                {
+                    lbMovie.Items.Add(item.Title.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("Your search didn't return any result!");
+                    tbName.Text = "";
+                }
+            }
+                
+        }
+
+       
+
+        private void lbMovie_SelectedValueChanged(object sender, EventArgs e)
+        {
+            RestClient restClient = new RestClient($"http://www.omdbapi.com/?apikey=98f50a84&");
+            RestRequest restRequest = new RestRequest(Method.GET);
+            restRequest.AddParameter("t", lbMovie.SelectedItem.ToString());
+           
+            var result = restClient.Execute(restRequest);
+            RestSharp.Deserializers.JsonDeserializer deserial = new RestSharp.Deserializers.JsonDeserializer();
+            Movie movie = deserial.Deserialize<Movie>(result);
+            pb.ImageLocation = movie.Poster;
+            rtbDetails.Text = "Year: " + movie.Year + "\nActors: " + movie.Actors + "\nGenre: " + movie.Genre + "\nWriter: " + movie.Writer;
+            rtbPlot.Text = "Plot: \n" + movie.Plot;
+
+
+        }
+
+        private void tbName_TextChanged(object sender, EventArgs e)
+        {
+            btnSearch.Enabled = true;
+        }
+    }
+}
